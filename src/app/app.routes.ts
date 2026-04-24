f35ee21464bb925237881e343edbe825eef264dd
elmarzmo <mohamed1elmarzougui@gmail.com>
2026-04-24 00:26:47 -0500

    feat: add services route to application routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { Contact } from './pages/contact/contact';
+import { Services } from './pages/services/services';
 export const routes: Routes = [
   { path: '', component: Home },
-
+  { path: 'services', component: Services },
   { path: 'contact', component: Contact },
   { path: '**', redirectTo: '' },
 ];
